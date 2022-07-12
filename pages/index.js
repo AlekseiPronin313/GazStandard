4fd0a4a1c8bdbcb168e39560639b6ebdc2f69175
Pronin <isw.pronin@yandex.ru>
2022-07-12 23:42:43 +0300

    fix swiper

--- pages/index.js
+++ pages/index.js
@@ -24,16 +24,12 @@ ymaps.ready(init);
 
 let swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
-  slidesPerView: 4,
+  slidesPerView: 5,
   freeMode: true,
   watchSlidesProgress: true,
 });
 let swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   thumbs: {
     swiper: swiper,
   },
