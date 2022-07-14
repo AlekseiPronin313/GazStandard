e52859d4a9a64f53a669ab738d9c679f11e55130
Pronin <isw.pronin@yandex.ru>
2022-07-15 00:19:06 +0300

    popup

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,32 @@
 let center = [54.616998, 39.730248];
+const openPopup = document.querySelectorAll(".products_button");
+const closePopup = document.querySelector("#close-popup");
+const popup = document.querySelector(".popup");
+
+function open() {
+  popup.classList.add("popup__opened");
+  document.addEventListener("keydown", function escClose(evt) {
+    if (evt.key === "Escape") {
+      close();
+      document.removeEventListener("keydown", escClose);
+    }
+  });
+}
+
+function close() {
+  popup.classList.remove("popup__opened");
+}
+
+openPopup.forEach((botton) => {
+  botton.addEventListener("click", open);
+});
+closePopup.addEventListener("click", close);
+
+document.addEventListener("click", (evt) => {
+  if (evt.target.classList.contains("popup__overlay")) {
+    close();
+  }
+});
 
 function init() {
   let map = new ymaps.Map("map", {
